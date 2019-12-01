0f3c7c83f74876b43f21b521dc1a115aca97241f
Brad Purchase <bradpurchase@gmail.com>
2019-12-01 12:09:03 -0500

    convert indentation to spaces

--- day-1-the-tyranny-of-the-rocket-equation/main.go
+++ day-1-the-tyranny-of-the-rocket-equation/main.go
@@ -1,49 +1,49 @@
 package main
 
 import (
-	"bufio"
-	"fmt"
-	"os"
-	"strconv"
+  "bufio"
+  "fmt"
+  "os"
+  "strconv"
 )
 
 func readInputFile(filePath string) ([]int) {
-	file, err := os.Open(filePath)
-	if err != nil {
-		panic(err)
-	}
-	defer file.Close()
+  file, err := os.Open(filePath)
+  if err != nil {
+    panic(err)
+  }
+  defer file.Close()
 
-	scanner := bufio.NewScanner(file)
-	var nums []int
-	for scanner.Scan() {
-		text, err := strconv.Atoi(scanner.Text())
-		if err != nil {
-			panic(err)
-		}
-		nums = append(nums, text)
-	}
-	return nums
+  scanner := bufio.NewScanner(file)
+  var nums []int
+  for scanner.Scan() {
+    text, err := strconv.Atoi(scanner.Text())
+    if err != nil {
+      panic(err)
+    }
+    nums = append(nums, text)
+  }
+  return nums
 }
 
 func calculateFuel(mass int) int {
-	return ((mass / 3) - 2)
+  return ((mass / 3) - 2)
 }
 
 func main() {
-	nums := readInputFile("input.txt")
-	total, totalFuelSums := 0, 0
-	for _, mass := range nums {
-		total += calculateFuel(mass)
-		for {
-			fuel := calculateFuel(mass)
-			if fuel <= 0 {
-				break
-			}
-			totalFuelSums += fuel
-			mass = fuel
-		}
-	}
-	fmt.Println("part one =", total)
-	fmt.Println("part two =", totalFuelSums)
+  nums := readInputFile("input.txt")
+  total, totalFuelSums := 0, 0
+  for _, mass := range nums {
+    total += calculateFuel(mass)
+    for {
+      fuel := calculateFuel(mass)
+      if fuel <= 0 {
+        break
+      }
+      totalFuelSums += fuel
+      mass = fuel
+    }
+  }
+  fmt.Println("part one =", total)
+  fmt.Println("part two =", totalFuelSums)
 }
